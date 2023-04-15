b65d9f7ee8bec67e80aec357454851f3e4769e19
Андрей Соловов <solovov2003@yandex.ru>
2023-04-15 21:36:42 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        DataTable dt;
+        SqlConnection connection = new SqlConnection(@"Data Source=DESKTOP-AVGELME\STP;Initial Catalog=root;Integrated Security=True");
+
+        int countClick = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Interval = 20000;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dt = new DataTable();
+
+            countClick++;
+            if (countClick == 3)
+            {
+                this.Enabled = false;
+                countClick = 0;
+                timer1.Start();
+                new FormCaptha().Show();
+            }
+
+            connection.Open();
+
+            string login = textBox1.Text;
+            string password = textBox2.Text;
+
+            SqlCommand cmd = new SqlCommand($"select * from users where login = '{login}' and password = '{password}'", connection);
+            dt.Load(cmd.ExecuteReader());
+
+            if (dt.Rows.Count > 0)
+            {
+                MessageBox.Show("Вы успешно авторизовались");
+            }
+
+            else
+            {
+                MessageBox.Show("Такого пользователя не существует, попробуйте еще раз");
+            }
+
+            connection.Close();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.Enabled = true;
+            timer1.Stop();
+        }
+    }
+}
---
+++ WindowsFormsApp3/FormCaptha.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class FormCaptha : Form
+    {
+        string str = GetCaptha();
+        public FormCaptha()
+        {
+            InitializeComponent();
+        }
+
+        public static string GetCaptha()
+        {
+            string result = "";
+            char[] chars = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm".ToArray();
+            Random random = new Random();
+
+            for (int i = 0; i < 5; i++)
+            {
+                result += chars[random.Next(0, chars.Length - 1)];
+            }
+
+            return result;
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Font font = new Font("Times New Roman", 14);
+
+            e.Graphics.Clear(Color.Black);
+            e.Graphics.DrawString(str, font, Brushes.White, 10, 10);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text == str)
+            {
+                this.Close();
+            }
+        }
+    }
+}
